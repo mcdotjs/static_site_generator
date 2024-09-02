d7435a1891b5815a7d484e0616ae3e3355b071ee
mcdotjs <mirectm@gmail.com>
2024-09-02 21:43:14 +0200

    textnode to leafnode .... skopirovane testy

--- src/main.py
+++ src/main.py
@@ -5,23 +5,25 @@ from htmlnode import ParentNode
 
 
 def main():
-    a = TextNode("This is a text node", "bold", "https: // www.boot.dev")
-    attrbts = {
-        "href": "https://www.google.com",
-        "target": "_blank",
-    }
-    node = HTMLNode("h1", "test", [], attrbts)
-    leafnode = LeafNode("a", "Mirko attributes ?????", attrbts)
+    a = TextNode("This is a text node", "image", "https://www.google.com")
+    print(a.text_node_to_html_node().__repr__())
+    # attrbts = {
+    #     "href": "https://www.google.com",
+    #     "target": "_blank",
+    # }
+    # node = HTMLNode("h1", "test", [], attrbts)
+    # leafnode = LeafNode("a", "Mirko attributes ?????", attrbts)
+    #
+    # P = ParentNode(
+    #     "p",
+    #     [
+    #         LeafNode("b", "Bold text"),
+    #         LeafNode(None, "Normal text"),
+    #         LeafNode("i", "italic text"),
+    #         LeafNode(None, "Normal text"),
+    #     ],
+    # )
+    # print(P.to_html())
 
-    P = ParentNode(
-        "p",
-        [
-            LeafNode("b", "Bold text"),
-            LeafNode(None, "Normal text"),
-            LeafNode("i", "italic text"),
-            LeafNode(None, "Normal text"),
-        ],
-    )
-    print(P.to_html())
 
 main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,4 @@
+from htmlnode import LeafNode
 text_type_text = "text"
 text_type_bold = "bold"
 text_type_italic = "italic"
@@ -21,3 +22,35 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+    def text_node___it_has_to_be_standalone_function____not_method(self):
+        if self.text_type not in [text_type_text, text_type_bold, text_type_italic, text_type_code, text_type_link, text_type_image]:
+            raise ValueError(f"Invalid text type: {self.text_type}")
+        elif self.text_type is text_type_text:
+            return LeafNode(None, self.text)
+        elif self.text_type is text_type_bold:
+            return LeafNode("b", self.text)
+        elif self.text_type is text_type_italic:
+            return LeafNode("i", self.text)
+        elif self.text_type is text_type_code:
+            return LeafNode("code", self.text)
+        elif self.text_type is text_type_link:
+            return LeafNode("a", self.text, {'href': self.url})
+        elif self.text_type is text_type_image:
+            return LeafNode("img", self.text, {'src': self.url, 'alt': self.text})
+
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == text_type_text:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == text_type_bold:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == text_type_italic:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == text_type_code:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == text_type_link:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == text_type_image:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"Invalid text type: {text_node.text_type}")
